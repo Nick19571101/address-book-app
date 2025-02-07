b142bb878ea79ec4b86e857f4313136493fd062e
Nick19571101 <nkv682594@gmail.com>
2025-02-07 18:41:48 +0200

    done new commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/address-book-app',
 	plugins: [react()],
 	extends: ['eslint:all', 'plugin:react/all'],
 	// parserOptions: {
