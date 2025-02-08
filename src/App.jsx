be0efbdc5afe20e7c1b64e2c814199afb08f30ce
Nick19571101 <nkv682594@gmail.com>
2025-02-08 17:53:00 +0200

    create search-contact component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,26 @@ import './App.css'
 import CreateContactForm from './components/contactForm/CreateContactForm'
 import ContactDetails from './components/ContactDetails'
 import EditContactForm from './components/editContact/EditContactForm'
+import SearchInput from './components/searchContact/SearchInput'
+
+const filterContacts = (contacts, filteredValue) =>
+	contacts.filter(
+		contact =>
+			contact.firstName.toLowerCase().includes(filteredValue.toLowerCase()) ||
+			contact.lastName.toLowerCase().includes(filteredValue.toLowerCase()),
+	)
 
 function App() {
 	const [contactList, setContactList] = useState([])
 	const [appState, setAppState] = useState('default')
+	const [searchValue, setSearchValue] = useState('')
 	const [activeContactId, setActiveContactId] = useState(null)
+
+	const activeContact = contactList.find(
+		contact => contact.id === activeContactId,
+	)
+	const filteredContacts = filterContacts(contactList, searchValue)
+	const isEmptySearch = searchValue.length > 0 && filteredContacts.length === 0
 
 	const handleAddNewClick = () => {
 		setAppState('create_contact')
@@ -22,9 +37,6 @@ function App() {
 		setAppState('view_contact')
 		setActiveContactId(contactId)
 	}
-	const activeContact = contactList.find(
-		contact => contact.id === activeContactId,
-	)
 	const handleEditContact = () => {
 		setAppState('edit_contact')
 	}
@@ -42,39 +54,48 @@ function App() {
 		setContactList(newContactsList)
 		setAppState('view_contact')
 	}
+	const handleSearchChange = event => {
+		setSearchValue(event.target.value)
+	}
 
 	return (
 		<>
 			<div className='address__book'>
 				<div className='address__book_left'>
-					{/* Header (в майбутньому сюди додамо ще search input) */}
+					<div className='search_input'>
+						<SearchInput value={searchValue} onChange={handleSearchChange} />
+					</div>
 					<button className='btn btn-primary' onClick={handleAddNewClick}>
 						Add new
 					</button>
 					<ContactsList
-						contacts={contactList}
+						contacts={filteredContacts}
 						onContactClick={handleContactClick}
 						activeContactId={activeContactId}
 					/>
 				</div>
-				<div className='address__book_right'>
-					{appState === 'create_contact' && (
-						<CreateContactForm onCreateContact={handleCreateContact} />
-					)}
-					{appState === 'view_contact' && (
-						<ContactDetails
-							contact={activeContact}
-							onEditContact={handleEditContact}
-						/>
-					)}
-					{appState === 'edit_contact' && (
-						<EditContactForm
-							contact={activeContact}
-							onCansel={handleCancelEditContact}
-							onUpdateContact={handleUpdateContact}
-						/>
-					)}
-				</div>
+				{isEmptySearch ? (
+					<h2>Empty search results</h2>
+				) : (
+					<div className='address__book_right'>
+						{appState === 'create_contact' && (
+							<CreateContactForm onCreateContact={handleCreateContact} />
+						)}
+						{appState === 'view_contact' && (
+							<ContactDetails
+								contact={activeContact}
+								onEditContact={handleEditContact}
+							/>
+						)}
+						{appState === 'edit_contact' && (
+							<EditContactForm
+								contact={activeContact}
+								onCansel={handleCancelEditContact}
+								onUpdateContact={handleUpdateContact}
+							/>
+						)}
+					</div>
+				)}
 			</div>
 		</>
 	)
